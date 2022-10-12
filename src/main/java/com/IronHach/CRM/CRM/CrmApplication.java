675904cf7ca0968ee9e26d075942ca498959e559
YehosuaEs <yehosuaes@gmail.com>
2022-10-12 18:14:30 +0200

    menu with reporting menu

--- src/main/java/com/IronHach/CRM/CRM/CrmApplication.java
+++ src/main/java/com/IronHach/CRM/CRM/CrmApplication.java
@@ -8,32 +8,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrmApplication implements CommandLineRunner {
 
-	@Override
-	public void run(String... args) throws Exception {
-		String action = "";
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
+    }
 
 
-		Boolean quitApp = true;
-		try {
-			Steps.greeting();
-			while (quitApp) {
-				action = Steps.menu();
-				quitApp = Steps.actions(action);
-				action = "";
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+    public void run(String... args) throws Exception {
+        String action = "";
 
 
+        Boolean quitApp = true;
+        try {
+            Steps.greeting();
+            while (quitApp) {
+                action = Steps.menu();
+                quitApp = Steps.actions(action);
+                action = "";
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-	}
 
+    }
 
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(CrmApplication.class, args);
-	}
 }
 
--- src/main/java/com/IronHach/CRM/CRM/models/Steps.java
+++ src/main/java/com/IronHach/CRM/CRM/models/Steps.java
@@ -41,17 +41,17 @@ public class Steps {
         System.out.println(ANSI_YELLOW +
                 "\n                 PLEASE TYPE WHAT DO YOU WANT TO DO" + ANSI_YELLOW +
                 "\n---------------------------------------------------------------------" + ANSI_RESET +
-                "\n1.-Type 'New Lead' to add a new lead" +
+                "\n1.-Type 'New Lead' to add a new lead." +
                 "\n2.-Type 'Show Leads' to see list of leads you already have." +
                 "\n3.-Type 'Lookup lead <id>' to see to the info of an individual lead (<id> = id of the Lead)." +
                 "\n4.-Type 'Convert <id>' to Contact to see the info of an individual contact (<id> = id of the lead). " +
                 "\n5.-Type 'Create opportunity'" +
-                "\n6.-Type 'show opportunity' to display the opportunities you already have" +
-                "\n7.-Type 'opportunity status' to close the opportunity WON or LOST" +
-                "\n8.-Type 'create account' to display the opportunities you already have" +
-                "\n9.-Type 'show account' to display the opportunities you already have" +
-                "\n10.-Type 'Quit' to close the application."
-
+                "\n6.-Type 'show opportunity' to display the opportunities you already have." +
+                "\n7.-Type 'opportunity status' to close the opportunity WON or LOST." +
+                "\n8.-Type 'create account' to display the opportunities you already have." +
+                "\n9.-Type 'show account' to display the opportunities you already have." +
+                "\n9.-Type 'Reporting Functionality' to display Reporting Functionality 📈 menu." +
+                "\n42.-Type 'Quit' to close the application."
         );
         while (answermenuInput.isEmpty()) {
             answermenuInput = scanner.nextLine();
@@ -113,6 +113,10 @@ public class Steps {
                 System.out.println("You type show account");
                 Methods.showAccount();
                 return true;
+            case "reporting functionality":
+                System.out.println("You type reporting functionality");
+                reports();
+                return true;
             case "quit":
                 System.out.println("\033[0;1m" + "You type quit");
                 System.out.println("\033[0;1m" + "Thanks for  using CRM APP");
@@ -123,4 +127,71 @@ public class Steps {
                 return true;
         }
     }
+
+
+    public static void reports() {
+
+    }
+
+    public static void menuReports(){
+        String answermenuInput = "";
+        System.out.println(ANSI_RED_BACKGROUND + "                        * CRM APPLICATION *                          " + ANSI_RED_BACKGROUND + ANSI_RESET);
+        System.out.println(ANSI_YELLOW +
+                "\n                 PLEASE TYPE WHAT DO YOU WANT TO DO" + ANSI_YELLOW +
+                "\n---------------------------------------------------------------------" + ANSI_RESET +
+                "\n - - - - - - - - - -  Reporting Functionality 📈- - - - - - - - - - - -" +
+                "\n -   -   -   -   -   -   -   - SalesRep  📊 -   -   -   -   -   -   -   -" +
+                "\n10.-Type 'Report Lead by SalesRep' to display the count of Leads by SalesRep." +
+                "\n11.-Type 'Report Opportunity by SalesRep' to display count of all Opportunities by SalesRep." +
+                "\n12.-Type 'Report CLOSED-WON by SalesRep' to display the count of all CLOSED_WON Opportunities by SalesRep. " +
+                "\n13.-Type 'Report CLOSED-LOST by SalesRep' to display the count of all CLOSED_LOST Opportunities by SalesRep." +
+                "\n14.-Type 'Report OPEN by SalesRep' to display the count of all OPEN Opportunities by SalesRep." +
+                "\n -   -   -   -   -   -   -   - Product 📦 -   -   -   -   -   -   -   -" +
+                "\n15.-Type 'Report Opportunity by the product' to display the count of all Opportunities by the product." +
+                "\n16.-Type 'Report CLOSED-WON by the product' to display count of all CLOSED_WON Opportunities by the product." +
+                "\n17.-Type 'Report CLOSED-LOST by the product' to display the count of all CLOSED_LOST Opportunities by the product." +
+                "\n18.-Type 'Report OPEN by the product' to display the count of all OPEN Opportunities by the product." +
+                "\n -   -   -   -   -   -   -   - Country 📍 -   -   -   -   -   -   -   -" +
+                "\n19.-Type 'Report Opportunity by Country' to display the count of all Opportunities by country." +
+                "\n20.-Type 'Report CLOSED-WON by Country' to display the count of all CLOSED_WON Opportunities by country." +
+                "\n21.-Type 'Report CLOSED-LOST by Country' to display the count of all CLOSED_LOST Opportunities by country." +
+                "\n22.-Type 'Report OPEN by Country' to display the count of all OPEN Opportunities by country." +
+                "\n -   -   -   -   -   -   -   -  City 🏢  -   -   -   -   -   -   -   -" +
+                "\n23.-Type 'Report Opportunity by City' to display the count of all Opportunities by the city." +
+                "\n24.-Type 'Report CLOSED-WON by City' to display the count of all CLOSED_WON Opportunities by the city." +
+                "\n25.-Type 'Report CLOSED-LOST by City' to display the count of all CLOSED_LOST Opportunities by the city." +
+                "\n26.-Type 'Report OPEN by City' to display the count of all OPEN Opportunities by the city." +
+                "\n -   -   -   -   -   -   -    Industry  ⚙️   -   -   -   -   -   -   -" +
+                "\n27.-Type 'Report Opportunity by Industry' to display the count of all Opportunities by industry." +
+                "\n28.-Type 'Report CLOSED_WON by Industry' to display the count of all CLOSED_WON Opportunities by industry." +
+                "\n29.-Type 'Report CLOSED_LOST by Industry' to display the count of all CLOSED_LOST Opportunities by industry." +
+                "\n30.-Type 'Report OPEN by Industry' to display the count of all OPEN Opportunities by industry." +
+                "\n -   -   -   -   -   -   -  EmployeeCount 👩🏽‍💼🧑🏻‍💼 -   -   -   -   -   -   -" +
+                "\n30.-Type 'Mean EmployeeCount' to display the mean employeeCount." +
+                "\n31.-Type 'Median EmployeeCount' to display the median employeeCount." +
+                "\n32.-Type 'Max EmployeeCount' to display the maximum employeeCount." +
+                "\n33.-Type 'Min EmployeeCount' to display the minimum employeeCount." +
+                "\n -   -   -   -   -   -   -    Quantity  🧮📦   -   -   -   -   -   -   -" +
+                "\n34.-Type 'Mean Quantity' to display the mean products order." +
+                "\n35.-Type 'Median Quantity' to display the minimum products order." +
+                "\n36.-Type 'Max Quantity' to display the minimum products order." +
+                "\n37.-Type 'Min Quantity' to display the minimum products order." +
+                "\n -   -   -   -   -   -   -   Opportunity 🛒  -   -   -   -   -   -   -" +
+                "\n38.-Type 'Mean Opps per Account”' to display the mean of Opportunities associated with an Account." +
+                "\n39.-Type 'Median Opps per Account”' to display the minimum of Opportunities associated with an Account." +
+                "\n40.-Type 'Max Opps per Account”' to display the minimum of Opportunities associated with an Account." +
+                "\n41.-Type 'Min Opps per Account”' to display the minimum of Opportunities associated with an Account." +
+                "\n41.-Type 'main menu”' to display the main menu" +
+                "\n42.-Type 'Quit' to close the application."
+        );
+        while (answermenuInput.isEmpty()) {
+            answermenuInput = scanner.nextLine();
+            if (answermenuInput.isEmpty()) {
+                System.out.println("Please verify you type the action correctly");
+            }
+        }
+        //return answermenuInput;
+    }
+
+
 }
